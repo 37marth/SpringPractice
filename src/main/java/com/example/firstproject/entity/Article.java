3f38e3aed1f0736edf754214c0f4c95fa0386a98
김현기 <dnflgusrl167@gmail.com>
2026-07-09 03:35:17 +0900

    로깅 설정 및 db url설정과 실습

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,10 +1,7 @@
 package com.example.firstproject.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -14,7 +11,7 @@ import lombok.*;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)//db가 id 자동생성
     private Long id;
     @Column
     private String title;
